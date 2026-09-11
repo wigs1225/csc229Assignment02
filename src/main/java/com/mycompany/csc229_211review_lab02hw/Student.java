54760d2893ea5e070367cd40b13a419bb3790b0f
Robby Wigand <n00952206@students.ncc.edu>
2026-09-11 18:05:28 -0400

    Add gpa getter/setter

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,25 +4,55 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
+//"extends Person" makes Student a child of person.
 public class Student extends Person{
 	// ToDo 1: Make this class a child of Person
 	// ToDo 2: Fix the resulting errors
 
+    //address and gpa variables.
     private String address;
     double gpa;
 	// ToDo 3: Add a field for GPA and create setter and getter
 
+    //Constructor
     Student(String name, short age, double gpa){
+        //super calls the constructor of the person class and gives it name and age.
         super(name, age);
         this.gpa = gpa;
     }
 
+    /**
+     * public String getAddress()
+     * required by the parent to have this method.
+     * @return address
+     */
     public String getAddress(){
         return address;
     }
+    /**
+     * public String setAddress()
+     * required by the parent to have this method.
+     * sets address for the student
+     */
     public void setAddress(String address){
         this.address = address;
     }
+
+    /**
+     * public String getGpa()
+     * @return gpa
+     */
+    public double getGpa(){
+        return gpa;
+    }
+    /**
+     * public String setGpa()
+     * sets gpa for the student
+     */
+    public void setGpa(double gpa){
+        this.gpa = gpa;
+    }
 	// ToDo 4: Add comments to your code
+
 
 }
